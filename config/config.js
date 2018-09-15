1486ee65118f716d93a41e585cb30460bab02f78
hao.xie <hao.xie@hzztai.com>
2018-09-15 20:36:12 +0800

    model

--- config/config.js
+++ config/config.js
@@ -2,6 +2,27 @@ export default {
     singular: true,  //让page（单数）变成约定的存放页面的文件夹
     routes: [{
         path: '/',
-        component: './HelloWorld',
-    }]
+        component: 'Layout/BasicLayout',
+        //indexRoute: { redirect: '/HelloWorld' },
+        routes: [
+            { path:'/HelloWorld', component:'HelloWorld' },
+            { path:'/dashboard/analysis', component:'Dashboard/Analysis' },
+            { path:'/dashboard/monitor', component:'Dashboard/Monitor' },
+            { path:'/dashboard/workplace', component:'Dashboard/Workplace' },
+            { path:'puzzlecards', component: './puzzlecards' },
+            ],
+        /*indexRoute: { component:'./src/page/HelloWorld' },
+        childRoutes: [
+            {
+                path: 'HelloWorld',
+                component: './src/page/HelloWorld'
+            }
+        ]*/
+    }],
+    plugins: [
+        ['umi-plugin-react', {
+            antd: true,
+            dva: true,
+        }],
+    ],
 };
---
+++ src/page/Dashboard/Analysis.js
@@ -0,0 +1,3 @@
+export default () =>(
+    <h1>Dashboard Analysis Page</h1>
+)
